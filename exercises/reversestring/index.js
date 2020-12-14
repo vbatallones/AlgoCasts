13e7a2afd8033035aa82ab22c66636ba0a59d9c9
Levin <vbatallones.xvi@gmail.com>
2020-12-13 19:10:07 -0800

    solving the reversestring problem

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,16 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    /* 
+        I split on each character. The split method will make the string into an array.
+
+        After I split the string into each character and it's an array already, now I used the
+        reverse() method which will reverse the array of each character.
+        
+        Then I use the join() method to join them after reversing the string.
+    */
+    return str.split('').reverse().join("")
+}
 
 module.exports = reverse;
