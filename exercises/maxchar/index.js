6a5a375d97bccbbcd19103d5fe2533c0b210dffe
Levin <vbatallones.xvi@gmail.com>
2020-12-14 21:17:11 -0800

    add sudo code for better understanding

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -6,24 +6,34 @@
 // maxChar("apple 1231111") === "1"
 
 function maxChar(str) {
+    // create a variable and set it into an empty obj.
     const obj = {};
+    // setting a max variable to zero to compare if the element greater than max
     let max = 0;
+    // a maxChar variable set into empty string where we will passed in the element that is commonly used.
     let maxChar = '';
 
+    // iterate over the set of string
     for (let char of str) {
-        if (!obj[char]) {
-            obj[char] = 1;
-        } else {
+        // if the character is more than one appearance we will increment the count
+        if (obj[char]) {
             obj[char]++;
+        } else {
+            // otherwise set it into one
+            obj[char] = 1;
         }
     }
-
+    // iterate over objects
     for (let key in obj) {
+        // if the value of the object property is greater than the max
+        // set the max to that highest value
+        // and set the maxChar to that key property
         if (obj[key] > max) {
             max = obj[key]
             maxChar = key
         }
     }
+    // return maxChar
     return maxChar
 
 }
