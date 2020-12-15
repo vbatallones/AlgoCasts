eb8162e8f31e99f328347b77dbbb78ce7aebbdf0
Levin <vbatallones.xvi@gmail.com>
2020-12-15 14:13:54 -0800

    solution for fizzbuzz problem

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,34 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+    let start = 1
+    while (start <= n){
+        if (start % 3 === 0 && start % 5 === 0) {
+            console.log('fizzbuzz')
+        } else if (start % 3 === 0) {
+            console.log('fizz')
+        } else if (start % 5 === 0) {
+            console.log('buzz')
+        } else {
+            console.log(start)
+        }
+        start++
+    }
+}
+
+function fizzBuzz(n) {
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log('FizzBuzz')
+        } else if (i % 3 === 0) {
+            console.log('Fizz')
+        } else if (i % 5 === 0) {
+            console.log('Buzz')
+        } else {
+            console.log(i)
+        }
+    }
+}
 
 module.exports = fizzBuzz;
